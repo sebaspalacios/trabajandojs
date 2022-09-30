f5888ba385450e52b2def435f7c82efa207e36bb
sebaspalacios <Sebapalacio08@hotmail.com>
2022-09-30 13:44:18 -0300

    aplicando cambios

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ let username = id("username"),
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+const nuevoContenido = document.createElement("div");
+nuevoContenido.innerHTML = `<h1> Hola ${username.value}! Tu registro fue exitoso</h1>`;
+nuevoContenido.className = "info-cliente";
+form.appendChild(nuevoContenido)
 
   engine(username, 0, "Debe colocar nombre de usuario");
-  if(username.value.length, email.value.length === 0) alert ("Complete sus datos")
+  if(username.value.length, email.value.length, password.value.length == 0) alert ("Complete sus datos")
   engine(email, 1, "Debe colocar su Email");
   engine(password, 2, "Su contraseña debe contener mas de 8 caracteres");
+  console.log(username.value, email.value, password.value)
 });
 
 let engine = (id, serial, message) => {
@@ -36,3 +41,5 @@ let engine = (id, serial, message) => {
     successIcon[serial].style.opacity = "1";
   }
 };
+localStorage.setItem("usuario", 100)
+console.log(localStorage.getItem("usuario"))
