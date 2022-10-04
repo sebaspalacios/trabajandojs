6efc7cd9890bf11e7934857a7ac2e28da15e9a73
sebaspalacios <Sebapalacio08@hotmail.com>
2022-10-04 19:15:04 -0300

    datos cambiados

--- app.js
+++ app.js
@@ -10,19 +10,26 @@ let username = id("username"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 const nuevoContenido = document.createElement("div");
-nuevoContenido.innerHTML = `<h1> Hola ${username.value}! Tu registro fue exitoso</h1>`;
-nuevoContenido.className = "info-cliente";
+nuevoContenido.innerHTML = `<div class="card">
+<div class="card-info">
+  <p class="text-title"> Hola ${username.value}</p>
+  <p class="text-body">¡Tu registro en MiTrabajo fue exitoso!</p>
+</div></div>`;
 form.appendChild(nuevoContenido)
 
   engine(username, 0, "Debe colocar nombre de usuario");
-  if(username.value.length, email.value.length, password.value.length == 0) alert ("Complete sus datos")
-  engine(email, 1, "Debe colocar su Email");
-  engine(password, 2, "Su contraseña debe contener mas de 8 caracteres");
+  if(username.value.length, email.value.length, password.value.length  == " ")  alert ("Debe completar los campos")
+  engine(email , 1 , "Debe colocar su Email");
+  engine(password, 2 , "Su contraseña debe contener mas de 8 caracteres");
   console.log(username.value, email.value, password.value)
 });
+
+localStorage.setItem("usuario", 10)
+console.log(localStorage.getItem("usuario"))
+
 
 let engine = (id, serial, message) => {
   if (id.value.trim() === "") {
@@ -41,5 +48,33 @@ let engine = (id, serial, message) => {
     successIcon[serial].style.opacity = "1";
   }
 };
-localStorage.setItem("usuario", 100)
-console.log(localStorage.getItem("usuario"))
+
+
+const button = document.getElementById('mandar')
+ button.addEventListener ('click',() => {
+const newPost = {
+"nombre de usuario": "Sebastian11",
+"email" : "Sebaspalacioss08@hotmail.com",
+"direccion" : " falucho 208",
+"edad" : 25 ,
+"ciudad" : "Cordoba",
+"pais" : "Argentina",
+"codigo posta" : 5000,
+}
+fetch('https://jsonplaceholder.typicode.com/users/' , {
+method: 'POST',
+body: JSON.stringify(newPost),
+headers: { "content-type": "posts/"
+ }
+}) 
+.then(data => console.log(newPost))
+ })  
+
+
+
+/* const procesaDatos = (e)=> {
+  e.preventDefault();
+  let datos = new FormData (e.target);
+  let datosCompletos = Object.fromEntries(datos.entries ())
+  console.log(JSON.stringify(datosCompletos))
+  form.addEventListener('submit', procesaDatos) */
